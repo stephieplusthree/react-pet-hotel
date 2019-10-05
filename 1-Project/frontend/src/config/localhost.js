b62ef2a28994f3ae0417c7580bc2f7ad0a284801
Stephanie <stephieplusthree@gmail.com>
2019-10-04 20:53:37 -0700

    first commit

--- 1-Project/frontend/src/config/localhost.js
+++ 1-Project/frontend/src/config/localhost.js
@@ -1,4 +1,13 @@
-const firebaseConfig = <insert firebase config here>;
+const firebaseConfig = {
+  apiKey: "AIzaSyAVwtBwCpHGPJSymqv3MbEb9mwO2shRIcY",
+  authDomain: "pet-hotel-prod-29b28.firebaseapp.com",
+  databaseURL: "https://pet-hotel-prod-29b28.firebaseio.com",
+  projectId: "pet-hotel-prod-29b28",
+  storageBucket: "pet-hotel-prod-29b28.appspot.com",
+  messagingSenderId: "421010757440",
+  appId: "1:421010757440:web:30c15a7fae2f9ad8dd4ca5",
+  measurementId: "G-Q7KCFN1JNL"
+};
 
 // Cloud Functions
 // const backendUrl = `http://localhost:5000/${
